01789cbb910022d98eece736a924752e6a001df7
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-09 14:32:01 +0300

    developing repositories on backend some bootstrap ops for frontend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductCategoryRepository.java
@@ -2,11 +2,13 @@ package com.ecommerce.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.springbootecommerce.entity.ProductCategory;
 
 // collectionResourceRel : name of JSON entry , path = restApi endpoint
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
--- 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
@@ -2,10 +2,12 @@ package com.ecommerce.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.springbootecommerce.entity.Product;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
