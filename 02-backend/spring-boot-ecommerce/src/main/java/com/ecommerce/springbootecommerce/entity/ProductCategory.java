b1d54cb897b0649b0a4d5c9ee2def78340368adb
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-09 09:43:11 +0300

    Creating entities- Product and Product category

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/entity/ProductCategory.java
@@ -0,0 +1,57 @@
+package com.ecommerce.springbootecommerce.entity;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class ProductCategory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String categoryName;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+	private Set<Product> products;
+
+	public ProductCategory(Long id, String categoryName) {
+		super();
+		this.id = id;
+		this.categoryName = categoryName;
+	}
+
+	public ProductCategory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+}
