4a1c52d443e5f72702fdebee09b49cb71be6d229
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-05-10 10:49:39 +0300

    expose ids for all entity types

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/springbootecommerce/dao/ProductRepository.java
@@ -1,13 +1,18 @@
 package com.ecommerce.springbootecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommerce.springbootecommerce.entity.Product;
 
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
